c40e52e5aac8aa8c338ef5c8885467a619476712
tom <tommy.surjadi@gmail.com>
2019-03-20 07:37:18 -0700

    Amended to show console.log

--- dogForm.js
+++ dogForm.js
@@ -1,3 +1,4 @@
+
 let formElement = document.querySelector('form');
 
 console.log(formElement);
@@ -5,9 +6,13 @@ console.log(formElement);
 formElement.addEventListener('submit', function(e) {
 e.preventDefault();
 const inputValue = formElement.elements[0].value;
+const inputValue2 = formElement.elements[1].value;
+const inputValue3 = formElement.elements[2].value;
 
 const dataToSubmit = {
-    name: inputValue
+    firstName: inputValue,
+    lastName: inputValue2,
+    email: inputValue3,
 }
 console.log(dataToSubmit);
 })
